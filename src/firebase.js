7785073ce39f7d290935ebfd48739527c4b11178
Phoenix-Dev2 <kaziromail@gmail.com>
2023-09-17 01:43:52 +0300

    update .env

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDKckXeXlxdxKqoAQND6KfuZOQGsTvXawM',
-  authDomain: 'chat-525dc.firebaseapp.com',
-  projectId: 'chat-525dc',
-  storageBucket: 'chat-525dc.appspot.com',
-  messagingSenderId: '923422556830',
-  appId: '1:923422556830:web:651255d7b3c3866d82b18f',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGEING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 // Initialize Firebase
