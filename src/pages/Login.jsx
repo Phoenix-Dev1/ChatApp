be394bb2c8c1b857f7f95b8742e6ae9ebcb8cb58
Phoenix-Dev2 <kaziromail@gmail.com>
2023-09-16 20:13:43 +0300

    Login & Register - Design
    completed Login and Register Design

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Kaziro Chat</span>
+        <span className="title">Login</span>
+        <form>
+          <input type="email" placeholder="Email" name="" id="" />
+          <input type="password" placeholder="Password" name="" id="" />
+          <button>Sign in</button>
+        </form>
+        <p>Don't have an account? Register</p>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import AddAvater from '../img/addAvatar.png';
 
 const Register = () => {
   return (
@@ -10,10 +11,14 @@ const Register = () => {
           <input type="text" placeholder="Display name" name="" id="" />
           <input type="email" placeholder="Email" name="" id="" />
           <input type="password" placeholder="Password" name="" id="" />
-          <input type="file" name="" id="" />
+          <input style={{ display: 'none' }} type="file" name="" id="file" />
+          <label htmlFor="file">
+            <img src={AddAvater} alt="" />
+            <span>Add an avatar</span>
+          </label>
           <button>Sign up</button>
         </form>
-        <p>Do you have an account? Lgin</p>
+        <p>Do you have an account? Login</p>
       </div>
     </div>
   );
